2efcd2e2ca52ff6711e4cdebf9a2c47aaf94bab3
Valeriy Malyshev <vmalyshev@bitwolff.com>
2019-05-08 15:38:34 +0400

    Add Material UI top bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from 'components/Header';
+
+import Dashboard from '../views/Dashboard';
 
 const App = () =>
-  <h1>Hello</h1>;
+  <BrowserRouter>
+    <Fragment>
+        <Header />
+
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
+
+    </Fragment>
+  </BrowserRouter>
 
 export default App;
